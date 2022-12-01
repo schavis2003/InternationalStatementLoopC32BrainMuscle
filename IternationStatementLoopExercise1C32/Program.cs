06078a6503e2837803dc11e752c70a679230d099
schavis2022 <schavis2022>
2022-12-01 12:45:14 -0500

    IternationStatementC32 BM

---
+++ IternationStatementLoopExercise1C32/Program.cs
@@ -0,0 +1,54 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+for (int i = 0; i <= 10; i++) 
+{
+    Console.WriteLine($"{i}");
+}
+
+for (int i = 0; i<= 10; i++)
+{
+    Console.WriteLine($"{i}");
+    
+}
+
+for (int i = 0; i <= 10; i++)
+    Console.WriteLine($"{i}");
+
+for (int i = 0; i <= 10; i++)
+    Console.WriteLine($"{i}");
+
+for (int i = 0; i <= 10; i++)
+    Console.WriteLine($"{i}");
+
+for (int i = 0; i <= 10; i++)
+    Console.WriteLine($"{i}");
+
+for (int i = 0; i <= 10; i++)
+    Console.WriteLine($"{i}");
+
+for (int i = 0; i <= 10; i++)
+    Console.WriteLine($"{i}");
+
+for (int i = 0; i <= 10; i++)
+    Console.WriteLine($"{i}");
+
+for (int i = 0; i <= 10; i++)
+    Console.WriteLine($"{i}");
+
+for (int i = 0; i <= 10; i++)
+    Console.WriteLine($"{i}");
+
+for (int i = 0; i <= 10; i++)
+    Console.WriteLine($"{i}");
+
+for (int i = 0; i <= 10; i++)
+    Console.WriteLine($"{i}");
+
+for (int i = 0; i <= 10; i++)
+    Console.WriteLine($"{i}");
+
+for (int i = 0; i <= 10; i++)
+    Console.WriteLine($"{i}");
+
+Console.WriteLine("Brain Muscle");
